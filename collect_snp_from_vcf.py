c1847d65c4d24d3c5c89f4651dc625b36f9a5388
Ольга Полещук <36551175+polololya@users.noreply.github.com>
2018-04-10 16:10:34 +0300

    Update collect_snp_from_vcf.py
    updated

--- collect_snp_from_vcf.py
+++ collect_snp_from_vcf.py
@@ -5,13 +5,12 @@ Created on Fri Mar  9 15:26:19 2018
 
 @author: mac
 """
-
-with open ('1001genomes_snp-short-indel_with_tair10_only_ACGTN.vcf', 'r') as vcf:
+with open ('ANNOTATIONS/arab_SNP.recode.vcf', 'r') as vcf:
     with open('SNPS.txt', 'w') as SNPS:
         counter = 0
         SNPS.write('"num"\t"global"\t"coords"\t"Chr_snp"\t"from"\t"to"')
         for line in vcf:
-            if (line[0:1] == '##') or (line[0:2] == "POS"):
+            if (line[0] == '#'):
                 continue
             else:
                 counter += 1
@@ -19,7 +18,8 @@ with open ('1001genomes_snp-short-indel_with_tair10_only_ACGTN.vcf', 'r') as vcf
                 if line[5] == ".":
                     continue
                 else:
-                    SNPS.write(counter+'\t'+line_list[1]+'\t'+line_list[1]+'\t'+line_list[0]+'\t'+line_list[3]+line_list[4])
-            
+                    #print(str(counter)+'\t'+str(line_list[1])+'\t'+str(line_list[1])+'\t'+str(line_list[0])+'\t'+str(line_list[3])+str(line_list[4]))
+                    SNPS.write(str(counter)+'\t'+str(line_list[1])+'\t'+str(line_list[1])+'\t'+str(line_list[0])+'\t'+str(line_list[3])+str(line_list[4]))
+
         
         
