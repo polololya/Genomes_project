c062eb19515f2f57b6262a03c1b2e48687847124
Ольга Полещук <36551175+polololya@users.noreply.github.com>
2018-04-10 15:20:20 +0300

    Update collect_snp_from_vcf.py
    updated code

--- collect_snp_from_vcf.py
+++ collect_snp_from_vcf.py
@@ -9,14 +9,17 @@ Created on Fri Mar  9 15:26:19 2018
 with open ('1001genomes_snp-short-indel_with_tair10_only_ACGTN.vcf', 'r') as vcf:
     with open('SNPS.txt', 'w') as SNPS:
         counter = 0
-        SNPS.write('"global"\t"coords"\t"Chr_snp"\t"from"\t"to"')
+        SNPS.write('"num"\t"global"\t"coords"\t"Chr_snp"\t"from"\t"to"')
         for line in vcf:
-            if line[0] == '##':
+            if (line[0:1] == '##') or (line[0:2] == "POS"):
                 continue
             else:
                 counter += 1
                 line_list = line.split('\t')
-                SNPS.write(counter+'\t'+line_list[1]+'\t'+line_list[1]+'\t'+line_list[0]+'\t'+line_list[3]+line_list[4])
+                if line[5] == ".":
+                    continue
+                else:
+                    SNPS.write(counter+'\t'+line_list[1]+'\t'+line_list[1]+'\t'+line_list[0]+'\t'+line_list[3]+line_list[4])
             
         
         
